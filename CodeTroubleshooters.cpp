3200c8fc17cdf0859a08b7dd3cb2374d96dc6410
adnan0944 <adnan20049@gmail.com>
2014-12-04 21:09:34 +0600

    Update CodeTroubleshooters.cpp

--- CodeTroubleshooters.cpp
+++ CodeTroubleshooters.cpp
@@ -53,6 +53,20 @@ void MatrixMultiplication(int a[MatDimension][MatDimension],
         }
 }
 
+// Function to return trailing 0s in factorial of n
+int findTrailingZeros(int  n)
+{
+    // Initialize result
+    int count = 0;
+
+    // Keep dividing n by powers of 5 and update count
+    for (int i=5; n/i>=1; i *= 5)
+        count += n/i;
+
+    return count;
+}
+
+
 /************************************************Data Structure***********************************************/
 
 void PreProcessRMQ(int M[MAXN][LOGMAXN],int A[MAXN],int N){ //O(NlogN)
